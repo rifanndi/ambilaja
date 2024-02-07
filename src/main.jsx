1733ff56e7d2afdd668c1bff26d0db015b4cb6f5
rifanndi <rifandiannass@gmail.com>
2024-02-07 18:20:36 +0700

    Pesan commit semoga bisa

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App.jsx";
-import "./style..css";
+import "./style.css"; // Path ke file CSS disesuaikan dengan struktur proyek
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
